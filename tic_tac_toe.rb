bc89bb7914dab6cbbfc708a14c80b30833098d43
katie <katiepb525@gmail.com>
2022-11-10 11:02:01 -0600

    create classes for player, grid, newgame

---
+++ tic_tac_toe.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# class that creates player instance
+class Player
+  def initialize(name, x_or_o)
+    # whats the player's name?
+    @name = name
+    # are they using 'X' or 'O'?
+    @x_or_o = x_or_o
+  end
+end
+
+# class that creates grid instance
+class Grid
+  def initialize
+    # grid to be displayed
+    @display_grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    # grid to keep track of Xs and Os
+    @curr_game = [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]]
+  end
+end
+
+# class that creates instance of a new game
+class NewGame
+  def initialize(player1, player2, game_grid)
+    @player1 = player1
+    @player2 = player2
+    @game_grid = game_grid
+  end
+end
