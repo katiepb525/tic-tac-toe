c90a3cf223c77d7f7ab65fd0a042b16cc8b2898f
katie <katiepb525@gmail.com>
2022-11-12 14:49:51 -0600

    make symbol choice implicit (player1 will always be X, player2 will always be O)

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -129,18 +129,16 @@ class NewGame
 end
 
 # get name of first player
-puts 'player 1, enter your name!'
+puts 'player 1, enter your name! youll be X...'
 p1_name = gets.chomp
-puts 'enter your symbol:'
-p1_symbol = gets.chomp
 p1 = Player.new(p1_name, 'X')
 
 # get name of second player
-puts 'player 2, enter your name!'
-p2_name = gets.chomp
+puts 'player 2, enter your name! youll be O...'
+p2_name = 'dan'
 p2 = Player.new(p2_name, 'O')
 
 grid = Grid.new
 
 game = NewGame.new(p1, p2, grid)
-game.play_round(p1)
+game.play_game
