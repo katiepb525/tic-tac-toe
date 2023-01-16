1081f42166047392cb5afe57e2bff2c631f1d13b
katie <katiepb525@gmail.com>
2023-01-15 18:24:54 -0600

    change subject var 'win_row' to clearer 'won_game'

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -9,56 +9,55 @@ describe NewGame do
     let(:grid){double('grid')}
 
     # initalize subject var
-    subject(:win_row){described_class.new(current_player, second_player, grid)}
+    subject(:won_game){described_class.new(current_player, second_player, grid)}
 
     context 'the player wins a row' do
       before do
-        allow(win_row).to receive(:won_row?).and_return(true)
-        allow(win_row).to receive(:won_column?).and_return(false)
-        allow(win_row).to receive(:won_diag?).and_return(false)
+        allow(won_game).to receive(:won_row?).and_return(true)
+        allow(won_game).to receive(:won_column?).and_return(false)
+        allow(won_game).to receive(:won_diag?).and_return(false)
       end
 
       it 'changes game_end to true' do
-        expect{win_row.player_won?(current_player)}.to change{win_row.instance_variable_get(:@game_end)}.to be(true)
+        expect{won_game.player_won?(current_player)}.to change{won_game.instance_variable_get(:@game_end)}.to be(true)
       end
     end
 
     context 'the player wins a column' do
       before do
-        allow(win_row).to receive(:won_row?).and_return(false)
-        allow(win_row).to receive(:won_column?).and_return(true)
-        allow(win_row).to receive(:won_diag?).and_return(false)
+        allow(won_game).to receive(:won_row?).and_return(false)
+        allow(won_game).to receive(:won_column?).and_return(true)
+        allow(won_game).to receive(:won_diag?).and_return(false)
       end
 
       it 'changes game_end to true' do
-        expect{win_row.player_won?(current_player)}.to change{win_row.instance_variable_get(:@game_end)}.to be(true)
+        expect{won_game.player_won?(current_player)}.to change{won_game.instance_variable_get(:@game_end)}.to be(true)
       end
     end
 
     context 'the player wins a diagonal' do
       before do
-        allow(win_row).to receive(:won_row?).and_return(false)
-        allow(win_row).to receive(:won_column?).and_return(false)
-        allow(win_row).to receive(:won_diag?).and_return(true)
+        allow(won_game).to receive(:won_row?).and_return(false)
+        allow(won_game).to receive(:won_column?).and_return(false)
+        allow(won_game).to receive(:won_diag?).and_return(true)
       end
 
       it 'changes game_end to true' do
-        expect{win_row.player_won?(current_player)}.to change{win_row.instance_variable_get(:@game_end)}.to be(true)
+        expect{won_game.player_won?(current_player)}.to change{won_game.instance_variable_get(:@game_end)}.to be(true)
       end
     end
 
     context 'the player does not win' do
       before do
-        allow(win_row).to receive(:won_row?).and_return(false)
-        allow(win_row).to receive(:won_column?).and_return(false)
-        allow(win_row).to receive(:won_diag?).and_return(false)
+        allow(won_game).to receive(:won_row?).and_return(false)
+        allow(won_game).to receive(:won_column?).and_return(false)
+        allow(won_game).to receive(:won_diag?).and_return(false)
       end
 
       it 'does not change game_end to true' do
-        expect{win_row.player_won?(current_player)}.not_to change{win_row.instance_variable_get(:@game_end)}
+        expect{won_game.player_won?(current_player)}.not_to change{won_game.instance_variable_get(:@game_end)}
       end
     end
-    
   end
 
 end
