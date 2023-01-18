ac5690dda0d66a2813ed4d68c3d77e371a37c3dc
katie <katiepb525@gmail.com>
2023-01-17 21:22:07 -0600

    cleanup with DRY principle and rubocop

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,247 +1,171 @@
+# frozen_string_literal: true
+
 require './lib/tic_tac_toe'
 
 describe NewGame do
+  # double for player class
+  let(:current_player) { double('player', name: 'Jessie', symbol: 'X') }
+  let(:second_player) { double('player', name: 'James', symbol: 'O') }
+
+  # double for grid class
+  let(:grid) { double('grid', place: [1, 2, 3, 4, 5, 6, 7, 8, 9]) }
+
+  # initialize subject var
+  subject(:game) { described_class.new(current_player, second_player, grid) }
 
   ######### tests for win conditions #########
 
   describe '#player_won?' do
-    # double for player class
-    let(:current_player){ double('player', name: 'Jessie')}
-    let(:second_player){ double('player', name: 'James')}
-    # double for grid class
-    let(:grid){double('grid')}
-
-    # initialize subject var
-    subject(:won_game){described_class.new(current_player, second_player, grid)}
-
     context 'the player wins a row' do
       before do
-        allow(won_game).to receive(:won_row?).and_return(true)
-        allow(won_game).to receive(:won_column?).and_return(false)
-        allow(won_game).to receive(:won_diag?).and_return(false)
+        allow(game).to receive(:won_row?).and_return(true)
       end
 
       it 'changes game_end to true' do
-        expect{won_game.player_won?(current_player)}.to change{won_game.instance_variable_get(:@game_end)}.to be(true)
+        expect { game.player_won?(current_player) }.to change { game.instance_variable_get(:@game_end) }.to be(true)
       end
     end
 
     context 'the player wins a column' do
       before do
-        allow(won_game).to receive(:won_row?).and_return(false)
-        allow(won_game).to receive(:won_column?).and_return(true)
-        allow(won_game).to receive(:won_diag?).and_return(false)
+        allow(game).to receive(:won_column?).and_return(true)
       end
 
       it 'changes game_end to true' do
-        expect{won_game.player_won?(current_player)}.to change{won_game.instance_variable_get(:@game_end)}.to be(true)
+        expect { game.player_won?(current_player) }.to change { game.instance_variable_get(:@game_end) }.to be(true)
       end
     end
 
     context 'the player wins a diagonal' do
       before do
-        allow(won_game).to receive(:won_row?).and_return(false)
-        allow(won_game).to receive(:won_column?).and_return(false)
-        allow(won_game).to receive(:won_diag?).and_return(true)
+        allow(game).to receive(:won_diag?).and_return(true)
       end
 
       it 'changes game_end to true' do
-        expect{won_game.player_won?(current_player)}.to change{won_game.instance_variable_get(:@game_end)}.to be(true)
+        expect { game.player_won?(current_player) }.to change { game.instance_variable_get(:@game_end) }.to be(true)
       end
     end
 
     context 'the player does not win' do
-      before do
-        allow(won_game).to receive(:won_row?).and_return(false)
-        allow(won_game).to receive(:won_column?).and_return(false)
-        allow(won_game).to receive(:won_diag?).and_return(false)
-      end
-
       it 'does not change game_end to true' do
-        expect{won_game.player_won?(current_player)}.not_to change{won_game.instance_variable_get(:@game_end)}
+        expect { game.player_won?(current_player) }.not_to change { game.instance_variable_get(:@game_end) }
       end
     end
   end
 
   describe '#won_row?' do
-    # double for player class
-    let(:current_player){ double('player', name: 'Jessie', symbol: 'X')}
-    let(:second_player){ double('player', name: 'James', symbol: 'O')}
-
     context 'all of first row contains current players symbol' do
-      # double for grid class with first row full of X's
-      let(:grid){double('grid', place: ['X','X','X',4,5,6,7,8,9])}
-      # initialize subject var
-      subject(:won_row){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: ['X', 'X', 'X', 4, 5, 6, 7, 8, 9]) }
 
       it 'returns true' do
-        expect(won_row.won_row?(current_player)).to be(true)
+        expect(game.won_row?(current_player)).to be(true)
       end
     end
 
     context 'all of second row contains current players symbol' do
-      # double for grid class with second row full of X's
-      let(:grid){double('grid', place: [1,2,3,'X','X','X',7,8,9])}
-      # initialize subject var
-      subject(:won_row){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: [1, 2, 3, 'X', 'X', 'X', 7, 8, 9]) }
 
       it 'returns true' do
-        expect(won_row.won_row?(current_player)).to be(true)
+        expect(game.won_row?(current_player)).to be(true)
       end
     end
 
     context 'all of third row contains current players symbol' do
-      # double for grid class with third row full of X's
-      let(:grid){double('grid', place: [1,2,3,4,5,6,'X','X','X'])}
-      
-      # initialize subject var
-      subject(:won_row){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: [1, 2, 3, 4, 5, 6, 'X', 'X', 'X']) }
 
       it 'returns true' do
-        expect(won_row.won_row?(current_player)).to be(true)
+        expect(game.won_row?(current_player)).to be(true)
       end
     end
 
     context 'no winning rows on the grid' do
-      # double for grid class that has no winning rows
-      let(:grid){double('grid', place: [1,2,'O',4,5,6,'O','X','X'])}
-      
-      # initialize subject var
-      subject(:won_row){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: [1, 2, 'O', 4, 5, 6, 'O', 'X', 'X']) }
 
       it 'returns false' do
-        expect(won_row.won_row?(current_player)).to be(false)
+        expect(game.won_row?(current_player)).to be(false)
       end
     end
   end
 
   describe '#won_column?' do
-    # double for player class
-    let(:current_player){ double('player', name: 'Jessie', symbol: 'X')}
-    let(:second_player){ double('player', name: 'James')}
-
     context 'all of the first column contains current player symbol' do
-      # double for grid class that has first column full of X's
-      let(:grid){double('grid', place: ['X',2,3,'X',5,6,'X',8,9])}
-
-      # initialize subject var
-      subject(:won_column){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: ['X', 2, 3, 'X', 5, 6, 'X', 8, 9]) }
 
       it 'returns true' do
-        expect(won_column.won_column?(current_player)).to be(true)
+        expect(game.won_column?(current_player)).to be(true)
       end
     end
 
     context 'all of the second column contains current player symbol' do
-      # double for grid class that has first column full of X's
-      let(:grid){double('grid', place: [1,'X',3,4,'X',6,7,'X',9])}
-
-      # initialize subject var
-      subject(:won_column){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: [1, 'X', 3, 4, 'X', 6, 7, 'X', 9]) }
 
       it 'returns true' do
-        expect(won_column.won_column?(current_player)).to be(true)
+        expect(game.won_column?(current_player)).to be(true)
       end
     end
-    
-    context 'all of the third column contains current player symbol' do
-      # double for grid class that has first column full of X's
-      let(:grid){double('grid', place: [1,2,'X',4,5,'X',7,8,'X'])}
 
-      # initialize subject var
-      subject(:won_column){described_class.new(current_player, second_player, grid)}
+    context 'all of the third column contains current player symbol' do
+      let(:grid) { double('grid', place: [1, 2, 'X', 4, 5, 'X', 7, 8, 'X']) }
 
       it 'returns true' do
-        expect(won_column.won_column?(current_player)).to be(true)
+        expect(game.won_column?(current_player)).to be(true)
       end
     end
 
     context 'none of the columns contain current player symbol consecutively' do
-      # double for grid class that does not have winning column
-      let(:grid){double('grid', place: ['O',2,'O',4,5,'X',7,8,'X'])}
-
-      # initialize subject var
-      subject(:won_column){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: ['O', 2, 'O', 4, 5, 'X', 7, 8, 'X']) }
 
       it 'returns nil' do
-        expect(won_column.won_column?(current_player)).to be_nil
+        expect(game.won_column?(current_player)).to be_nil
       end
     end
   end
 
   describe '#won_diag?' do
-    # double for player class
-    let(:current_player){ double('player', name: 'Jessie', symbol: 'X')}
-    let(:second_player){ double('player', name: 'James', symbol: '0')}
-
     context 'first diagonal wins' do
-      # double for grid class that has first winning diagonal
-      let(:grid){double('grid', place: ['X',2,3,4,'X',6,7,8,'X'])}
-
-      # initialize subject var
-      subject(:won_diag){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: ['X', 2, 3, 4, 'X', 6, 7, 8, 'X']) }
 
       it 'returns true' do
-        expect(won_diag.won_diag?(current_player)).to be(true)
+        expect(game.won_diag?(current_player)).to be(true)
       end
     end
 
     context 'second diagonal wins' do
-      # double for grid class that has second winning diagonal
-      let(:grid){double('grid', place: [1,2,'X',4,'X',6,'X',8,9])}
-
-      # initialize subject var
-      subject(:won_diag){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: [1, 2, 'X', 4, 'X', 6, 'X', 8, 9]) }
 
       it 'returns true' do
-        expect(won_diag.won_diag?(current_player)).to be(true)
+        expect(game.won_diag?(current_player)).to be(true)
       end
     end
 
     context 'no diagonals win' do
-      # double for grid class that has no diagonals
-      let(:grid){double('grid', place: ['X',2,'O',4,'X',6,'X',8,9])}
-
-      # initialize subject var
-      subject(:won_diag){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: ['X', 2, 'O', 4, 'X', 6, 'X', 8, 9]) }
 
       it 'returns nil' do
-        expect(won_diag.won_diag?(current_player)).to be_nil
+        expect(game.won_diag?(current_player)).to be_nil
       end
     end
   end
 
   describe '#tie?' do
-    # double for player class
-    let(:current_player){ double('player', name: 'Jessie', symbol: 'X')}
-    let(:second_player){ double('player', name: 'James', symbol: '0')}
-
     context 'grid is fully marked' do
-      # double for fully marked grid
-      let(:grid){double('grid', place: ['X','X','O','O','X','O','X','O','O'])}
-     
-      # initialize subject var
-      subject(:tie){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: %w[X X O O X O X O O]) }
 
       it 'puts receives message' do
-        expect(tie).to receive(:puts).with('it was a tie!')
-        tie.tie?
+        expect(game).to receive(:puts).with('it was a tie!')
+        game.tie?
       end
 
       it 'changes game_end to true' do
-        expect{tie.tie?}.to change{tie.instance_variable_get(:@game_end)}.to(true)
+        expect { game.tie? }.to change { game.instance_variable_get(:@game_end) }.to(true)
       end
     end
 
     context 'grid is NOT fully marked' do
-      # double for not fully marked grid
-      let(:grid){double('grid', place: ['X','X',2,'O','X','O',6,'O','O'])}
-
-      # initialize subject var
-      subject(:tie){described_class.new(current_player, second_player, grid)}
+      let(:grid) { double('grid', place: ['X', 'X', 2, 'O', 'X', 'O', 6, 'O', 'O']) }
 
       it 'returns nil' do
-        expect(tie.tie?).to be_nil
+        expect(game.tie?).to be_nil
       end
     end
   end
@@ -249,67 +173,26 @@ describe NewGame do
   ####### tests for gameplay #######
 
   describe '#update_square' do
-    # double for player class
-    let(:current_player){ double('player', name: 'Jessie', symbol: 'X')}
-    let(:second_player){ double('player', name: 'James', symbol: 'O')}
-
-    # double for grid class
-    let(:grid){double('grid', place: [1,2,3,4,5,6,7,8,9])}
-
-    # initialize subject var
-    subject(:game){described_class.new(current_player, second_player, grid)}
-
     it 'updates grid.place[index] with current players symbol' do
       index = 1
-      expect{game.update_square(index, current_player)}.to change{grid.place[1]}.to eq("X")
+      expect { game.update_square(index, current_player) }.to change { grid.place[1] }.to eq('X')
     end
   end
 
   describe '#play_round' do
-    # double for player class
-    let(:current_player){ double('player', name: 'Jessie', symbol: 'X')}
-    let(:second_player){ double('player', name: 'James', symbol: 'O')}
-
-    # double for grid class
-    let(:grid){double('grid', place: [1,2,3,4,5,6,7,8,9])}
-
-    # initialize subject var
-    subject(:game){described_class.new(current_player, second_player, grid)}
-
-    context 'at the start of the game' do
-      before do
-        allow(grid).to receive(:display_board).and_return(true)
-      end
-
-      it 'displays the board' do
-        expect(grid).to receive(:display_board)
-        game.play_round(current_player)
-      end
-      
-      # it 'records player choice' do
-      #   # refactor choice to be an instance variable
-      # end  
-    
+    it 'displays the board' do
+      expect(grid).to receive(:display_board).twice
+      game.play_round(current_player)
     end
 
     # context 'chosen spot by player is not taken' do
-    #   it 'checks if spot is taken and returns false' do
-    #   end
-
-    #   it 'updates choice' do
-    #     # refactor choice
-    #   end
-
     #   it 'updates square with chosen spot' do
+
     #   end
     # end
 
     # context 'chosen spot by player IS taken' do
     #   it 'loops until player chooses open spot, then records choice' do
-    #   end
-
-    #   it 'updates choice' do
-    #     # refactor choice
     #   end
 
     #   it 'updates square with chosen spot' do
